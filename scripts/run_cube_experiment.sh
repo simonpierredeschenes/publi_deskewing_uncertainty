faa64aaf82e44672cf14c7d64a9fc9606e072d49
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2023-07-04 15:37:10 -0400

    Added run number to map and trajectory file names.

--- scripts/run_cube_experiment.sh
+++ scripts/run_cube_experiment.sh
@@ -19,12 +19,13 @@ matrix_file="$results_folder"/mapper.txt
 
 for file in `ls -v "$data_folder"`
 do
-  if [[ -f "$data_folder/$file/metadata.yaml" && $file = run_12 ]]
+  if [[ -f "$data_folder/$file/metadata.yaml" && $file = run_* ]]
   then
+    run_nb=`echo $file | grep -o "[0-9]*"`
     run_bag="$data_folder/$file"
     ros2 run imu_saturation imu_publisher_node --ros-args -r imu_in:=/MTI30_imu/data -r imu_out:=/MTI30_imu/data_interpolated -p imu_measurements_file_name:="$run_bag.csv" -p saturation_point:=5.0 &
     ros2 run utility_nodes waiting_node --ros-args -r topic_in:=imu_publisher_status
-    ros2 launch publi_deskewing_uncertainty cube_launch.xml bagfile:=$run_bag final_transformation_file_name:=$matrix_file use_icra_model:=false scale_factor:=$scale_factor use_interpolated_measurements:=true saturation_point:=5.0 &
+    ros2 launch publi_deskewing_uncertainty cube_launch.xml bagfile:=$run_bag final_transformation_file_name:=$matrix_file use_icra_model:=false scale_factor:=$scale_factor use_interpolated_measurements:=false saturation_point:=5.0 &
     sleep 10
 
     ros2 node list > /dev/null # force node discovery
@@ -34,9 +35,9 @@ do
     done
 
     ros2 service call /save_map map_msgs/srv/SaveMap "filename:
-  data: '$results_folder/map.vtk'"
+  data: '$results_folder/map_$run_nb.vtk'"
     ros2 service call /save_trajectory map_msgs/srv/SaveMap "filename:
-  data: '$results_folder/trajectory.vtk'"
+  data: '$results_folder/trajectory_$run_nb.vtk'"
 
     killall rviz2
     killall mapper_node
